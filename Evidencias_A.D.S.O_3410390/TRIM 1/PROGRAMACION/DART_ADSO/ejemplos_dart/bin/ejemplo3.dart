c80aa03c011ea59657fcd8efac02b256bce5b8d3
Cristian4201 <benavides4ca@gmail.com>
2026-05-05 11:13:40 -0500

    Ejrmplo dart estructuras secuenciales

---
+++ Evidencias_A.D.S.O_3410390/TRIM 1/PROGRAMACION/DART_ADSO/ejemplos_dart/bin/ejemplo3.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //DEFINICION DE VARIABLES
+  double base = 10, altura = 5, area;
+
+  //PROCESO
+  area = (base * altura) / 2;
+
+  //SALIDA
+  print("El area del triangulo es: $area"); 
+  
+}
