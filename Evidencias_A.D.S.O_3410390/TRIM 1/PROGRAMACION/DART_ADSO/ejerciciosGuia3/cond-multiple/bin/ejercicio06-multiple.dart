c13ca7d6eb340f02745d4d2e8b8a80212c39806f
Cristian4201 <benavides4ca@gmail.com>
2026-06-01 08:07:45 -0500

    Ejercicios condicionales

---
+++ Evidencias_A.D.S.O_3410390/TRIM 1/PROGRAMACION/DART_ADSO/ejerciciosGuia3/cond-multiple/bin/ejercicio06-multiple.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  /*6. Desarrollar un algoritmo que lea el nombre del mes y el año e imprima en pantalla cuantos días tiene.
+Es necesario tener en cuenta si es año bisiesto o no. */
+
+
+}
